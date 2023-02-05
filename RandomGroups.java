0c74798bb51b81ae39e30274a9babd353f830fa8
alothmanmj <alothman.maj@gmail.com>
2023-02-05 13:25:20 +0300

    build reader

--- RandomGroups.java
+++ RandomGroups.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class RandomGroups {
   public static void main(String[] args) {
     System.out.println("Main works");
-  }  
+    ArrayList arr = studentReader();
+    System.out.println(arr);
+  }
+  
+  public static ArrayList<String> studentReader(){
+    ArrayList studenArrayList = new ArrayList<>();
+
+    try {
+      File stufile = new File("Student List.txt");
+      Scanner sc = new Scanner(stufile);
+
+      while(sc.hasNextLine()){
+        studenArrayList.add(sc.nextLine());
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return studenArrayList;
+  }
 }
